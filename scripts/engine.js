8cec1c90c576e89dda4a2fcf84b44ad955d1808c
fernando <lfap19@gmail.com>
2024-01-27 10:46:24 -0300

    feat: inserido audios e função volume

---
+++ scripts/engine.js
@@ -0,0 +1,29 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input');
+
+let mapedKeys = [];
+let audio = new Audio("./tunes/./a.wav");
+
+const playTune = (key) => {
+    audio.src = `./tunes/${key}.wav`;
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add('active');
+    setTimeout(() => clickedKey.classList.remove('active'), 150);
+}
+
+pianoKeys.forEach((key) => {
+    key.addEventListener('click', () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener('keydown', (event) => {
+    if (mapedKeys.includes(event.key)) {
+        playTune(event.key);
+    }
+})
+
+volumeSlider.addEventListener('input', (event) => {
+    audio.volume = event.target.value
+})
